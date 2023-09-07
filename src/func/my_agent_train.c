358db7015d3fb33e4bf64275fec4dc1cede76368
FukuInTheCode <tomc.code@gmail.com>
2023-09-07 21:54:29 +0200

    add a set new qv

--- src/func/my_agent_train.c
+++ src/func/my_agent_train.c
@@ -11,3 +11,9 @@ uint32_t my_agent_get_action(my_agent_t *agent, uint32_t current_state,\
                                                 current_state, old_qv);
     }
 }
+
+void my_agent_set_new_qv(my_agent_t *agent, uint32_t current_state,\
+                                            uint32_t action, double new_qv)
+{
+    agent->q_table.arr[current_state][action] = new_qv;
+}
--- src/func/my_ql_train.c
+++ src/func/my_ql_train.c
@@ -1,24 +1,21 @@
 #include "../../includes/my.h"
 
+
+
 void my_ql_train(my_ql_t *ql)
 {
     uint32_t current_state = ql->env->starting_state;
     double explo_proba = ql->start_explo_proba;
     for (uint32_t i = 0; i < ql->episodes_n; ++i) {
-        // reset
-        // one episode
         for (uint32_t j = 0; j < ql->max_episode_steps; ++j) {
             uint32_t action = my_agent_get_action(ql->agent, current_state, explo_proba);
             double old_qv = ql->agent->q_table.arr[current_state][action];
             int next_state = ql->env->action_table.arr[current_state][action];
             if (next_state == -1)
                 continue;
-            // change the ql->agent->q_table based on the reward
             double reward = ql->env->reward_table.arr[next_state][0];
             double max_next_qv = my_matrix_maxrow(&ql->agent->q_table, next_state);
             double new_qv = (1 - ql->alpha) * old_qv + ql->alpha *(reward + ql->gamma * max_next_qv);
-            ql->agent->q_table.arr[current_state][action] = new_qv;
-            // set the agent
             if (reward != 0 && reward != 1)
                 break;
             current_state = next_state;
