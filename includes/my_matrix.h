235cddef317f895ddbbba6baf4cadfd0996b3283
FukuInTheCode <tomc.code@gmail.com>
2023-09-08 01:15:56 +0200

    chanegs in h

--- includes/my_matrix.h
+++ includes/my_matrix.h
@@ -113,6 +113,9 @@ uint32_t my_matrix_find_row_index(my_matrix_t *A, uint32_t n,  double x);
 void my_matrix_linspace(my_matrix_t *m, double start,\
                                 double stop, uint32_t num);
 void my_matrix_setall(my_matrix_t *A, double const  x);
+void my_matrix_transform(my_matrix_t *A, uint32_t m,\
+                            uint32_t n, my_matrix_t *result);
+void my_matrix_ravel(my_matrix_t *A, my_matrix_t *result);
 
 #ifdef MATRIX_INIT_STR
 
