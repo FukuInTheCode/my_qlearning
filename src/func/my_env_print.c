632e898c3762dbf62a17af69ea8cb5d58ddc01d1
FukuInTheCode <tomc.code@gmail.com>
2023-09-07 22:49:20 +0200

    add a print char for the env

--- src/func/my_env_print.c
+++ src/func/my_env_print.c
@@ -9,3 +9,13 @@ void my_env_print_reward(my_env_t *env)
         printf("\n");
     }
 }
+
+void my_env_print_char(my_env_t *env)
+{
+    for (uint32_t i = 0; i < env->grid_rows; ++i) {
+        printf("  ");
+        for (uint32_t j = 0; j < env->grid_cols; ++j)
+            printf("%c ", (int)(env->infos_table.arr[i * env->grid_rows + j][1]));
+        printf("\n");
+    }
+}
