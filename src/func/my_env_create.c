23d133778c5ebd3cc34f7457110554797cd2e5f6
FukuInTheCode <tomc.code@gmail.com>
2023-09-07 22:20:35 +0200

    changes

--- src/func/my_env_create.c
+++ src/func/my_env_create.c
@@ -3,10 +3,9 @@
 void my_env_create(my_env_t *env)
 {
     env->states_n = env->grid_cols * env->grid_rows;
-    my_matrix_create(env->states_n, 1, 1,\
+    my_matrix_create(env->states_n, 2, 1,\
                             &(env->infos_table));
 
     my_matrix_create(env->states_n, env->actions_n, 1,\
                                         &(env->action_table));
-
 }
