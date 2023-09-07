ecdafc669e5a42af996f65712aff38d30af007ed
FukuInTheCode <tomc.code@gmail.com>
2023-09-07 22:32:24 +0200

    bugs fixs

--- src/func/my_env_print.c
+++ src/func/my_env_print.c
@@ -2,9 +2,9 @@
 
 void my_env_print(my_env_t *env)
 {
-    for (uint32_t i = 0; i < grid_rows; ++i) {
+    for (uint32_t i = 0; i < env->grid_rows; ++i) {
         printf("  ");
-        for (uint32_t j = 0; j < grid_cols; ++j)
+        for (uint32_t j = 0; j < env->grid_cols; ++j)
             printf("%lf ", env->infos_table.arr[i + j][0]);
         printf("\n");
     }
