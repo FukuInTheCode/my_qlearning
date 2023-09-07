7adcbc215c2326373fae4a8a0eccfd1b3bf86a91
FukuInTheCode <tomc.code@gmail.com>
2023-09-07 16:18:59 +0200

    bug fix

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include "../includes/my.h"
 
 static void fillMatrix(my_matrix_t *action_table, uint32_t states_n, uint32_t actions_n) {
+    int grid_columns = 3;
+    int grid_rows = 3;
     for (uint32_t state = 0; state < states_n; state++) {
         for (uint32_t action = 0; action < actions_n; action++) {
             int resulting_state;
@@ -13,25 +15,25 @@ static void fillMatrix(my_matrix_t *action_table, uint32_t states_n, uint32_t ac
                 if (current_col < grid_columns - 1) {
                     resulting_state = state + 1;
                 } else {
-                    resulting_state = state;  // Can't move right if at the right edge
+                    resulting_state = -1;  // Can't move right if at the right edge
                 }
             } else if (action == 1) {  // "go down"
                 if (current_row < grid_rows - 1) {
                     resulting_state = state + grid_columns;
                 } else {
-                    resulting_state = state;  // Can't move down if at the bottom edge
+                    resulting_state = -1;  // Can't move down if at the bottom edge
                 }
             } else if (action == 2) {  // "go up"
                 if (current_row > 0) {
                     resulting_state = state - grid_columns;
                 } else {
-                    resulting_state = state;  // Can't move up if at the top edge
+                    resulting_state = -1;  // Can't move up if at the top edge
                 }
             } else if (action == 3) {  // "go left"
                 if (current_col > 0) {
                     resulting_state = state - 1;
                 } else {
-                    resulting_state = state;  // Can't move left if at the left edge
+                    resulting_state = -1;  // Can't move left if at the left edge
                 }
             }
 
@@ -62,6 +64,7 @@ int main(int argc, char* argv[]) {
     MAT_DECLA(action_table);
 
     my_matrix_create(states_n, actions_n, 1, &action_table);
+    fillMatrix(&action_table, states_n, actions_n);
     MAT_PRINT(action_table);
 
     MAT_FREE(q_table);
