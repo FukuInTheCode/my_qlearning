62bb7c4faf78c0151d8c545f842a0d59046e8290
FukuInTheCode <tomc.code@gmail.com>
2023-09-07 23:17:56 +0200

    add a agent get path

---
+++ src/func/my_agent_path.c
@@ -0,0 +1,11 @@
+#include "../../includes/my.h"
+
+void my_agent_get_path(my_agent_t *agent, uint32_t steps,\
+                            uint32_t **path, my_matrix_t *actions)
+{
+    for (uint32_t i = 1; i < steps; ++i) {
+        double max = my_matrix_maxrow(&(agent->q_table), (*path)[i - 1]);
+        uint32_t id = my_matrix_find_col_index(&(agent->q_table), max);
+        (*path)[i] = actions->arr[(*path)[i - 1]][id];
+    }
+}
